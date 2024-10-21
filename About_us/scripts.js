0abe89f52ed91649fc2e88193d7bcf54a717c051
nithin-0111 <146701694+nithin-0111@users.noreply.github.com>
2024-10-21 08:58:16 +0530

    Add files via upload

---
+++ About_us/scripts.js
@@ -0,0 +1,8 @@
+// Function to scroll to a specific section
+function scrollToSection(sectionID) {
+    document.getElementById(sectionID).scrollIntoView({ behavior: 'smooth' });
+  }
+
+function openPPT() {
+      window.open('https://1drv.ms/p/c/13b768d0284ac9ac/ESf1Aq2wKchDjyvrTDLSd9MBnn5NfepuPmG3VVJJ3_41dw?e=egeseP', '_blank');  // Replace with your actual PPT link
+    }
